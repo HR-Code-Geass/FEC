0647b7896afe8503a93b9fca9361d81b32f1c4a4
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-08 23:10:21 -0700

    Cleans up messy code

--- client/src/components/QandA/QuestionsList.js
+++ client/src/components/QandA/QuestionsList.js
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 
 import QuestionsListEntry from './QuestionsListEntry';
 
+const Button = styled.button`
+  background: transparent;
+  color: white;
+  border-radius: 3px;
+  background-color: #112D4E;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
 const QuestionsTable = styled.table`
   border: none;
   width: 100%;
@@ -16,8 +25,26 @@ const QATextColumn = styled.col`
   width: 100%;
 `;
 
-const QuestionsList = ({ questions }) => {
+const QuestionsList = ({ questions, productID }) => {
   const [displayLimit, setDisplayLimit] = useState(4); // number of questions to display
+
+  const MoreAnsweredQuestionsButton = (
+    <Button
+      type="button"
+      onClick={() => setDisplayLimit(Number.POSITIVE_INFINITY)}
+    >
+      MORE ANSWERED QUESTIONS
+    </Button>
+  );
+
+  const CollapseQuestionsButton = (
+    <Button
+      type="button"
+      onClick={() => setDisplayLimit(4)}
+    >
+      COLLAPSE QUESTIONS
+    </Button>
+  );
 
   return (
     <QuestionsTable>
@@ -29,29 +56,13 @@ const QuestionsList = ({ questions }) => {
         {questions?.map((q, i) => (
           (i >= displayLimit)
             ? null
-            : <QuestionsListEntry question={q} key={i} />
+            : <QuestionsListEntry question={q} key={i} productID={productID} />
         ))}
         <tr>
           <td colSpan="2" style={{ textAlign: 'center' }}>
             {questions.length > displayLimit
-              ? (
-                <button
-                  type="button"
-                  onClick={() => setDisplayLimit(Number.POSITIVE_INFINITY)}
-                >
-                  MORE ANSWERED QUESTIONS
-                </button>
-              )
-              : (questions.length > 2
-                && (
-                <button
-                  type="button"
-                  onClick={() => setDisplayLimit(4)}
-                >
-                  COLLAPSE QUESTIONS
-                </button>
-                ))
-                || null}
+              ? MoreAnsweredQuestionsButton
+              : (questions.length > 2 && CollapseQuestionsButton) || null}
           </td>
         </tr>
       </tbody>
