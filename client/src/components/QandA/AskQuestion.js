0503d23ad656580b2242a0d7715823dfca632f7c
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-09 00:23:51 -0700

    Adds prop type checking

--- client/src/components/QandA/AskQuestion.js
+++ client/src/components/QandA/AskQuestion.js
@@ -1,16 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Modal from '../Modal';
 import AskQuestionModalContent from './AskQuestionModalContent';
-
-const Button = styled.button`
-  background: transparent;
-  color: white;
-  border-radius: 3px;
-  background-color: #112D4E;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`;
+import { Button } from '../presentation/Button.styles';
 
 const AskQuestion = ({ productID }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,14 +16,21 @@ const AskQuestion = ({ productID }) => {
         && (
         <Modal
           title="Ask Your Question"
-          content={
-            <AskQuestionModalContent onClose={() => setIsModalOpen(false)} productID={productID} />
-          }
           onClose={() => setIsModalOpen(false)}
+          content={(
+            <AskQuestionModalContent
+              onClose={() => setIsModalOpen(false)}
+              productID={productID}
+            />
+          )}
         />
         )}
     </>
   );
+};
+
+AskQuestion.propTypes = {
+  productID: PropTypes.number.isRequired,
 };
 
 export default AskQuestion;
