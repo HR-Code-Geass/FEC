3ba43a46c4448ef41f8d13c1dc41fba2b4fd23b6
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-09 21:19:38 -0700

    Cleans up messy code

--- client/src/components/QandA/QuestionsList.js
+++ client/src/components/QandA/QuestionsList.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Button } from '../presentation/Button.styles';
 import QuestionsListEntry from './QuestionsListEntry';
 
 const QuestionsTable = styled.table`
@@ -29,49 +28,25 @@ const QLContainer = styled.div`
   text-align: center;
 `;
 
-const QuestionsList = ({ questions, displayLimit, productID }) =>
-// const [displayLimit, setDisplayLimit] = useState(2); // number of questions to display
-
-// const MoreAnsweredQuestionsButton = (
-//   <Button
-//     type="button"
-//     onClick={() => setDisplayLimit(Number.POSITIVE_INFINITY)}
-//   >
-//     MORE ANSWERED QUESTIONS
-//   </Button>
-// );
-
-// const CollapseQuestionsButton = (
-//   <Button
-//     type="button"
-//     onClick={() => setDisplayLimit(2)}
-//   >
-//     COLLAPSE QUESTIONS
-//   </Button>
-// );
-
-  (
-    <QLContainer>
-      <QAContainer>
-        <QuestionsTable>
-          <colgroup>
-            <QAColumn span="1" />
-            <QATextColumn span="1" />
-          </colgroup>
-          <tbody>
-            {questions?.map((q, i) => (
-              (i >= displayLimit)
-                ? null
-                : <QuestionsListEntry question={q} key={q.question_id} productID={productID} />
-            ))}
-          </tbody>
-        </QuestionsTable>
-      </QAContainer>
-      {/* {questions.length > displayLimit
-        ? MoreAnsweredQuestionsButton
-        : (questions.length > 2 && CollapseQuestionsButton) || null} */}
-    </QLContainer>
-  );
+const QuestionsList = ({ questions, displayLimit, productID }) => (
+  <QLContainer>
+    <QAContainer>
+      <QuestionsTable>
+        <colgroup>
+          <QAColumn span="1" />
+          <QATextColumn span="1" />
+        </colgroup>
+        <tbody>
+          {questions?.map((q, i) => (
+            (i >= displayLimit)
+              ? null
+              : <QuestionsListEntry question={q} key={q.question_id} productID={productID} />
+          ))}
+        </tbody>
+      </QuestionsTable>
+    </QAContainer>
+  </QLContainer>
+);
 QuestionsList.propTypes = {
   questions: PropTypes.instanceOf(Array).isRequired,
   displayLimit: PropTypes.number.isRequired,
